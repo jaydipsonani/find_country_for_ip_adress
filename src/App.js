e5ec9dfd76efe12d45e6b6bbf69f4bf1be5b4866
sonanijaydip <sonanijaydip4@gmail.com>
2024-06-07 11:36:53 +0530

    ip-adress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Home from './Home';
+import About from './About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  // const [ipAddress, setIpAddress] = useState('');
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch data only on initial render
+    axios.get(`http://ip-api.com/json/`)
+      .then((res) => {
+        console.log(res.data)
+        console.log(`Searched from country: ${res.data.country} `);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setError("Error fetching data. Please try again.");
+      });
+  }, []);
+
+  // const handleSearch = () => {
+  //   handleclear();
+  //   setIpAddress('');
+  //   setError(null);
+  // }
+
+  // const handleclear = () => {
+  //   setIpAddress('');
+  // }
+
+    return (
+      <>
+        <div className='App'>
+          {/* <input
+            type='text'
+            placeholder='Enter IP Address'
+            value={ipAddress}
+            onChange={(e) => setIpAddress(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button> */}
+
+          {error && <p>{error}</p>}
+
+          {data && (
+            <>
+              {/* <p>IP: {data.ip}</p> */}
+              <p>City: {data.city}</p>
+              <p>Country: {data.country}</p>
+              <p>latitude: {data.lat}</p>
+              <p>longitude: {data.lon}</p>
+
+              <Home />
+              <About />
+            </>
+          )}
+        </div>
+      </>
+    );
+
+}
 export default App;
